af0ca84cbee851ea0defa0110b2a10af1a5ba372
pbreads0316 <76706385+pbreads0316@users.noreply.github.com>
2021-01-21 12:23:13 -0500

    Add files via upload
    updated code to use mysql.connect
    removed pymysql per Bruce's comments that it no longer works in production

--- nci-seronet-data-validator-test.py
+++ nci-seronet-data-validator-test.py
@@ -2,14 +2,13 @@ import json
 import zipfile                 
 import difflib
 import os
-import pymysql
+import mysql.connector
 
 import re
 import csv
 from datetime import datetime
-import boto3                    #used to connect to aws servies
+import boto3                    #used to connect to aws services
 from io import BytesIO          #used to convert file into bytes in order to unzip
-from io import StringIO         
 import dateutil.tz    
 ###############################################################################################################################    
 import file_validator_object 
@@ -112,8 +111,9 @@ def lambda_handler(event, context):
         print(submission_error_list)
 ######################################################################################################################
 # open each file and compare header list to mysql database to validate all column names exist and spelled correctly
-    conn,file_dbname = connect_to_sql_database(host_client,pre_valid_db,user_name,user_password)
+    conn = connect_to_sql_database(host_client,pre_valid_db,user_name,user_password)
 
+    s3_file_path = CBC_submission_name+'/'+ CBC_submission_info + '/' + 'Submission_Errors'
     if conn == 0:
         write_submission_error_csv(s3_resource,output_bucket_name,s3_file_path,"Submission_Error_List.csv",submission_error_list)
         print("Unable to connect to mySQL database to preform column name validation.  Terminating Validation Process")
@@ -124,12 +124,11 @@ def lambda_handler(event, context):
         error_files = error_files[1:]
         listOfFileNames = [i for i in listOfFileNames if i not in error_files]
     
-    current_error_count = 0; part_count = [];  bio_count = []; 
+    current_error_count = 0;  current_demo = [];
     for test_name in enumerate(listOfFileNames):
         test_object = file_validator_object.Submitted_file(test_name[1],' ')                                       #create the file object
-        csv_string = load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,test_name[1])
+        test_object.load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,test_name[1])
         
-        test_object.get_csv_table(csv_string)                                                              #populate object with data
         if test_name[1]  == "Prior_Test_Results.csv" :
             mysql_table_list = ['Prior_Test_Result']
         elif test_name[1] == "Demographic_Data.csv":
@@ -165,13 +164,11 @@ def lambda_handler(event, context):
             print(test_name[1] + " was not found, unable to check")
             continue
         if test_name[1] != "submission_metadata.csv":
-            test_object.compare_csv_to_mysql(file_dbname,mysql_table_list,conn)  
+            test_object.compare_csv_to_mysql(pre_valid_db,mysql_table_list,conn) 
+        if test_name[1] == 'Demographic_Data.csv':
+            current_demo =  test_object.Data_Table
+            current_demo = current_demo['Research_Participant_ID'].tolist()
         
-        if "Research_Participant_ID" in test_object.Column_Header_List:
-            part_count = part_count + (test_object.Data_Table["Research_Participant_ID"]).tolist()
-        if "Biospecimen_ID" in test_object.Column_Header_List:
-            bio_count = bio_count + (test_object.Data_Table["Biospecimen_ID"]).tolist()
-            
         if len(test_object.header_name_validation) > 1: 
             submission_error_list.append(test_object.header_name_validation[1:][0])
             current_error_count = current_error_count + 1;
@@ -181,9 +178,6 @@ def lambda_handler(event, context):
         print("extra columns, missing or duplicate entries: " + str(current_error_count) + " errors were found. \n")
         error_count = error_count + current_error_count
         
-    uni_part_ids = len(set(part_count))
-    uni_bio_ids = len(set(bio_count))
-    
     current_error_count = 0
     if len(submitting_center) == 0:
         error_msg = "Submission_Metadata.csv was not found in submission zip file"
@@ -221,29 +215,19 @@ def lambda_handler(event, context):
     
     print("### Submission validation was sucessfull.  No Errors were found ###")
     print("### Proceeding to check each csv file for validation ###")
-    del bio_count,count_table,current_error_count,file_to_submit,i,indices,list_copy,matching_letters,Number_of_Biospecimens
-    del Number_of_Research_Participants,part_count,sequence,sort_idx,submit_file_count,submit_list,submit_to_file,test_name
-    del test_object,uni_id,valid,wrong_count,wrong_letters,uni_bio_ids,uni_part_ids
+    del count_table,current_error_count,file_to_submit,i,indices,list_copy,matching_letters,Number_of_Biospecimens
+    del Number_of_Research_Participants,sequence,sort_idx,submit_file_count,submit_list,submit_to_file,test_name
+    del test_object,uni_id,valid,wrong_count,wrong_letters
 #####################################################################################################
 ## if no submission errors, pull key peices from sql schema and import cbc id file 
-    pos_list,neg_list,ukn_list = file_validator_object.get_mysql_queries(file_dbname,conn,1)
-    assay_results,assay_target = file_validator_object.get_mysql_queries(file_dbname,conn,2)
-    participant_ids,biospec_ids = file_validator_object.get_mysql_queries(file_dbname,conn,3)
+    pos_list,neg_list           = file_validator_object.get_mysql_queries(pre_valid_db,conn,1)
+    assay_results,assay_target  = file_validator_object.get_mysql_queries(pre_valid_db,conn,2)
+    participant_ids,biospec_ids = file_validator_object.get_mysql_queries(pre_valid_db,conn,3)
     
-    if 'Demographic_Data.csv' in listOfFileNames:
-        csv_string = load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,'Demographic_Data.csv')
-
-        test_object = file_validator_object.Submitted_file('Demographic_Data.csv',' ')    
-        test_object.get_csv_table(csv_string)                                                              #populate object with data
-        
-        current_demo =  test_object.Data_Table
-        current_demo = current_demo['Research_Participant_ID'].tolist()
+    if len(current_demo) > 0:
         current_demo = (set(participant_ids['Research_Participant_ID'].tolist() + current_demo))
         current_demo = [x for x in current_demo if x == x]
-        del test_object
-    else:
-        current_demo = [];
-    
+
     sql_connect = conn.cursor()
     table_sql_str = ("SELECT * FROM `" + pre_valid_db + "`.`Seronet_CBC_ID`")
     query_res = sql_connect.execute(table_sql_str)
@@ -255,61 +239,69 @@ def lambda_handler(event, context):
     s3_file_path = CBC_submission_name+'/'+ CBC_submission_info + '/' + 'Validation_Errors'
 ######################################################################################################################################################################################################  
     if "Prior_Test_Results.csv" in listOfFileNames:
-        prior_valid_object = file_validator_object.Submitted_file("Prior_Test_Results.csv",'Research_Participant_ID')  #create the file object
-        csv_string = load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,'Prior_Test_Results.csv')
-        prior_valid_object.get_csv_table(csv_string) 
-        
-        pos_list,neg_list,unk_list,prior_valid_object = file_validator_object.split_participant_pos_neg_prior(prior_valid_object,pos_list,neg_list)  
+        prior_valid_object = file_validator_object.Submitted_file("Prior_Test_Results.csv",'Research_Participant_ID')  
+        prior_valid_object.load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,'Prior_Test_Results.csv')
+        pos_list,neg_list = file_validator_object.split_participant_pos_neg_prior(prior_valid_object,pos_list,neg_list)  
         prior_valid_object = prior_test_result_validator(prior_valid_object,neg_list,pos_list,re,valid_cbc_ids,current_demo)
         prior_valid_object.write_error_file("Prior_Test_Results_Errors_Found.csv",s3_resource,s3_file_path,output_bucket_name)
 ######################################################################################################################################################################################################  
     if "Demographic_Data.csv" in listOfFileNames:
-        demo_data_object = file_validator_object.Submitted_file("Demographic_Data.csv",'Research_Participant_ID')  #create the file object
-        csv_string = load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,'Demographic_Data.csv')
-        demo_data_object.get_csv_table(csv_string)                    
-        
+        demo_data_object = file_validator_object.Submitted_file("Demographic_Data.csv",'Research_Participant_ID') 
+        demo_data_object.load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,'Demographic_Data.csv')
+
         demo_data_object = demographic_data_validator(demo_data_object,neg_list,pos_list,re,valid_cbc_ids)
         demo_data_object.write_error_file("Demographic_Data_Errors_Found.csv",s3_resource,s3_file_path,output_bucket_name)
 ######################################################################################################################################################################################################  
     if "Biospecimen_Metadata.csv" in listOfFileNames:
-        Biospecimen_object = file_validator_object.Submitted_file("Biospecimen_Metadata.csv",'Biospecimen_ID')                   #create the file object
-        csv_string = load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,'Biospecimen_Metadata.csv')
-        Biospecimen_object.get_csv_table(csv_string)                    #populate object with data
+        Biospecimen_object = file_validator_object.Submitted_file("Biospecimen_Metadata.csv",'Biospecimen_ID')                  
+        Biospecimen_object.load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,'Biospecimen_Metadata.csv')
         Biospecimen_object.get_pos_neg_logic(pos_list,neg_list)
+        biospec_ids = biospec_ids.append(Biospecimen_object.Data_Table[['Biospecimen_ID','Biospecimen_Type']])
         
-        biospec_ids.append(Biospecimen_object.Data_Table[['Biospecimen_ID','Biospecimen_Type']])
         Biospecimen_object = Biospecimen_validator(Biospecimen_object,neg_list,pos_list,re,valid_cbc_ids,current_demo)
         Biospecimen_object.write_error_file("Biospecimen_Errors_Found.csv",s3_resource,s3_file_path,output_bucket_name)
 ############################################################################################################################### 
     if "Aliquot_Metadata.csv" in listOfFileNames:
         Aliquot_object = file_validator_object.Submitted_file("Aliquot_Metadata.csv",['Aliquot_ID','Biospecimen_ID'])                   
-        csv_string = load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,'Aliquot_Metadata.csv')
-        Aliquot_object.get_csv_table(csv_string)                 
+        Aliquot_object.load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,'Aliquot_Metadata.csv')
         Aliquot_object = other_files_validator(Aliquot_object,re,valid_cbc_ids,biospec_ids,"Aliquot_Errors_Found.csv")
+        Aliquot_object.write_error_file("Aliquot_Errors_Found.csv",s3_resource,s3_file_path,output_bucket_name)
 ###############################################################################################################################
     if "Equipment_Metadata.csv" in listOfFileNames:
         Equipment_object = file_validator_object.Submitted_file("Equipment_Metadata.csv",['Equipment_ID','Biospecimen_ID'])     
-        csv_string = load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,'Equipment_Metadata.csv')
-        Equipment_object.get_csv_table(csv_string) 
+        Equipment_object.load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,'Equipment_Metadata.csv')
         Equipment_object = other_files_validator(Equipment_object,re,valid_cbc_ids,biospec_ids,"Equipment_Errors_Found.csv")
+        Equipment_object.write_error_file("Equipment_Errors_Found.csv",s3_resource,s3_file_path,output_bucket_name)
 ###############################################################################################################################
     if "Reagent_Metadata.csv" in listOfFileNames:
         Reagent_object = file_validator_object.Submitted_file("Reagent_Metadata.csv",['Biospecimen_ID','Reagent_Name'])    
-        csv_string = load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,'Reagent_Metadata.csv')
-        Reagent_object.get_csv_table(csv_string) 
+        Reagent_object.load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,'Reagent_Metadata.csv')
         Reagent_object = other_files_validator(Reagent_object,re,valid_cbc_ids,biospec_ids,"Reagent_Errors_Found.csv")
+        Reagent_object.write_error_file("Reagent_Errors_Found.csv",s3_resource,s3_file_path,output_bucket_name)
 ###############################################################################################################################
     if "Consumable_Metadata.csv" in listOfFileNames:
         Consumable_object = file_validator_object.Submitted_file("Consumable_Metadata.csv",['Biospecimen_ID','Consumable_Name'])  
-        csv_string = load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,'Consumable_Metadata.csv')
-        Consumable_object.get_csv_table(csv_string) 
+        Consumable_object.load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,'Consumable_Metadata.csv')
         Consumable_object = other_files_validator(Consumable_object,re,valid_cbc_ids,biospec_ids,"Consumable_Errors_Found.csv")
+        Consumable_object.write_error_file("Consumable_Errors_Found.csv",s3_resource,s3_file_path,output_bucket_name)
 ###############################################################################################################################
- 
     print("Connection to RDS mysql instance is now closed")
+    print("Validation Function is Complete")
     conn.close
-    
     return{}
+###############################################################################################################################
+def connect_to_sql_database(host_client,dbname,user_name,user_password):       #connect to mySQL database 
+    conn = 0
+    
+    try:
+        conn = mysql.connector.connect(user=user_name, host=host_client, password=user_password, database= dbname)
+        print("SUCCESS: Connection to RDS mysql instance succeeded for file remover tables")
+    except mysql.connector.Error as err:
+        print(err)
+    return conn                #return connection variable to be used in queries
+    
+
+##writes text file if submitted file is not a zip or unable to open the zip file    
 def write_error_message(s3_resource,output_bucket_name,s3_file_path,error_msg):
     file_name = "Submission_Error.txt"
     lambda_path = "/tmp/" + file_name
@@ -320,35 +312,16 @@ def write_error_message(s3_resource,output_bucket_name,s3_file_path,error_msg):
     
     s3_file_path = s3_file_path + "/" + file_name    
     s3_resource.meta.client.upload_file(lambda_path, output_bucket_name, s3_file_path)
-    print("## Error was found in the Submission File. Ending Validation ##")
+    print("## Errors were found in the Submitted File. Ending Validation ##")
 
-def connect_to_sql_database(host_client,file_dbname,user_name,user_password):
-    conn = 0
-    port = 3306
-    try:
-        conn = pymysql.connect(host = host_client, user=user_name, password=user_password, db=file_dbname, connect_timeout=5)
-        print("SUCCESS: Connection to RDS mysql instance succeeded\n")
-    except:
-        print("ERROR: Unexpected error: Could not connect to MySql instance.\n")
-    return conn,file_dbname
-    
+##writes a csv file containing any errors found in submission validation
 def write_submission_error_csv(s3_resource,output_bucket_name,s3_file_path,file_name,list_of_elem):
     lambda_path = "/tmp/" + file_name
     
-    with open(lambda_path, 'w', newline='') as csvfile:      #w is new file, a+ is append to file
+    with open(lambda_path, 'w+', newline='') as csvfile:      #w is new file, a+ is append to file
          csv_writer = csv.writer(csvfile)
          for file_indx in enumerate(list_of_elem):
              csv_writer.writerow(file_indx[1])  
              
     s3_file_path = s3_file_path + "/" + file_name    
     s3_resource.meta.client.upload_file(lambda_path, output_bucket_name, s3_file_path)
-    
-def load_csv_file(s3_client,output_bucket_name,CBC_submission_name,CBC_submission_info,test_name):
-    new_key = CBC_submission_name+'/'+ CBC_submission_info +'/'+ 'Submitted_Files/'+ test_name
-    
-    csv_obj = s3_client.get_object(Bucket=output_bucket_name, Key=new_key)
-    body = csv_obj['Body']
-    csv_string = body.read().decode('utf-8')
-    
-    load_string_name = StringIO(csv_string)
-    return load_string_name 
